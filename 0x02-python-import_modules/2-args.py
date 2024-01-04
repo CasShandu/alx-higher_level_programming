411117a16fa601f64a1af105de525d8ca7feadec
CasShandu <cas.shandu@gmail.com>
2024-01-04 05:25:22 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
     from sys import argv
     y = len(argv) - 1
     if y < 1:
-        print("{} arguments:".format(y))
+        print("{} arguments.".format(y))
     elif y == 1:
-        print("{}argument:".format(y))
+        print("{} argument:".format(y))
     else:
         print("{} arguments:".format(y))
     for i in range(y):
