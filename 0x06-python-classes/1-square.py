5df9fa9450fb63eea8de42ab35a0d87f8870bfa6
CasShandu <cas.shandu@gmail.com>
2024-01-23 03:14:30 -0800

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 
-class Square:
+"""a class Square that defines a square by: (based on 0-square.py)."""
 
+
+class Square:
+    """allocates a value to the square."""
     def __init__(self, size):
+        """initialize a value to the new square.
+
+        Args:
+            size (int): The size fo the new square.
+            """
 
         self.__size = size
