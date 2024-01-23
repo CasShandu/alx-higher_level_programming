40ee16246079253b75ad76da4b9d9cf7b8fd211b
CasShandu <cas.shandu@gmail.com>
2024-01-23 03:28:52 -0800

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 
+"""a class Square that defines a square by: (based on 2-square.py)"""
+
+
 class Square:
+    """denotes a square"""
     def __init__(self, size=0):
+        """initialize a square
+
+        Args:
+            size (int): the size of the square."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
