8b12242ba71709e7a544c1caf24e7f0def8184fc
CasShandu <cas.shandu@gmail.com>
2024-01-23 03:21:22 -0800

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 
+"""a class Square that defines a square by: (based on 1-square.py)"""
+
+
 class Square:
+    """allocates a value of a square."""
 
     def __init__(self, size=0):
+        """initialize a new square.
 
+    Args:
+        size (int): the size of the new square."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
