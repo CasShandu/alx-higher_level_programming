d0a51d63006fe0cc39285348bd29a700f8b4c7f7
CasShandu <cas.shandu@gmail.com>
2024-03-13 09:38:13 -0700

    102-rating_shows

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -6,5 +6,5 @@
 SELECT title, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
-GROUP BY title
-GROUP BY rating DESC;
+GROUP BY `title`
+GROUP BY `rating` DESC;
