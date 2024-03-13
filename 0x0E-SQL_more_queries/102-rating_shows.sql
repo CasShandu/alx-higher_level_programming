5897516d8c748133c6f8d66937a6a641484d4ed0
CasShandu <cas.shandu@gmail.com>
2024-03-13 09:30:29 -0700

    102-rating_shows

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -5,7 +5,6 @@
 -- The database name will be passed as an argument of the mysql command
 SELECT title, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_shows
-LEFT JOIN tv_show_ratings
-ON tv_show_ratings.show_id = tv_shows.id
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY title
 GROUP BY rating DESC;
