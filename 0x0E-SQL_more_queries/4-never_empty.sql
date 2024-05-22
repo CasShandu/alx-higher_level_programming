edcd1cb5a90191b10631671be56a36affddfcee9
CasShandu <cas.shandu@gmail.com>
2024-05-22 07:28:25 -0700

    4-never_empty.sql

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,9 @@
+-- a script that creates the table unique_id on your MySQL server.
+-- description id INT default value 1, name VARCHAR(256)
+-- The database name will be passed as an argument of the mysql command
+-- If the database exists, your script should not fail
+
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+);
